d2b6055c029d958d6fe47129dd057270f0fe84f0
EmirlanDogdurbaev <jigit38@gmail.com>
2021-04-14 23:44:15 +0300

    getting started

--- src/App.js
+++ src/App.js
@@ -1,23 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { NavLink, Route, Switch } from "react-router-dom";
+import Logo from "./components/Logo/Logo";
+import Projects from "./components/Projects/Projects";
+import Services from "./components/Services/Services";
+import AboutUs from "./components/AboutUs/AboutUs";
+import Blog from "./components/Blog/Blog";
+import Contacts from "./components/Contacts/Contacts";
+import Number from "./components/Number/Number";
+import NavButton from "./components/NavButton/NavButton";
+import Home from "./components/Home/Home";
+import Section from "./components/Section/Section";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul className="ul">
+        <li className="li1">
+          <NavLink exact activeClassName="activeLink" to="/">
+            <Logo />
+          </NavLink>
+        </li>
+
+        <div className="language">
+          <li>
+            <NavLink activeClassName="activeLink" to="/ru">
+             <p> RU</p>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink activeClassName="activeLink" to="/eng">
+              <p>ENG</p>
+            </NavLink>
+          </li>
+        </div>
+
+        <div className="center">
+          <li>
+            <NavLink activeClassName="activeLink" to="/projects">
+              Projects
+              <Projects />
+            </NavLink>
+          </li>
+          <li>
+            <NavLink activeClassName="activeLink" to="/services">
+              Services
+              <Services />
+            </NavLink>
+          </li>
+          <li>
+            <NavLink activeClassName="activeLink" to="/aboutUs">
+              About Us <AboutUs />
+            </NavLink>
+          </li>
+          <li>
+            <NavLink activeClassName="activeLink" to="/blog">
+              Blog
+              <Blog />
+            </NavLink>
+          </li>
+          <li>
+            <NavLink activeClassName="activeLink" to="/contacts">
+              Contacts
+              <Contacts />
+            </NavLink>
+          </li>
+        </div>
+
+
+        <li>
+          <span>
+            <p>Call by phone</p> 
+            <NavLink activeClassName="activeLink" to="/number">
+              +996 771 11 33 55
+              <Number />
+            </NavLink>
+          </span>
+        </li>
+
+
+        <li>
+          <NavLink activeClassName="activeLink" to="./button">
+            <NavButton />
+          </NavLink>
+        </li>
+      </ul>
+      <main >
+        <Home />
+        <Section />
+      </main>
+
+      <Switch>
+        <Route path="/projects" component={Projects} />
+        <Route path="/services" component={Services} />
+        <Route path="/aboutUs" component={AboutUs} />
+        <Route path="/blog" component={Blog} />
+        <Route path="/contacts" component={Contacts} />
+        <Route path="/number" component={Number} />
+        <Route path="/navButton" component={NavButton} />
+
+        <Route exact path="/" component={Logo} />
+      </Switch>
     </div>
   );
 }
